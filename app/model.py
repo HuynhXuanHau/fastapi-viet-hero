7e0d9c447581b316af6acd22be82f7a0b5943ce4
HuynhXuanHau <xuanhau17052005@gmail.com>
2025-04-30 17:26:26 +0700

    fix load model

--- app/model.py
+++ app/model.py
@@ -2,10 +2,19 @@ import os
 import numpy as np
 from PIL import Image
 import tensorflow as tf
+import requests
+
 
 # Fixed path for Docker
 MODEL_PATH = os.path.join(os.path.dirname(__file__), 'models', 'resnet50_final_t4_optimized.keras')
+MODEL_URL = "https://link-to-your-model/resnet50_final_t4_optimized.keras"
 
+def download_model_if_missing():
+    if not os.path.exists(MODEL_PATH):
+        print("Model not found. Downloading...")
+        os.makedirs(os.path.dirname(MODEL_PATH), exist_ok=True)
+        with open(MODEL_PATH, "wb") as f:
+            f.write(requests.get(MODEL_URL).content)
 def load_resnet_model():
     try:
         # Load the model without compiling it
